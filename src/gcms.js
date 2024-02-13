4c8b255634135fdf90330ac702b65370225ceea7
Shiva Singh <shivasingh.200087@gmail.com>
2024-02-14 00:38:02 +0530

    Embed code model done

--- src/gcms.js
+++ src/gcms.js
@@ -14,19 +14,21 @@ import axios from 'axios';
 import moment from 'moment';
 import { MdCopyAll } from 'react-icons/md';
 import copy from 'copy-to-clipboard';
-import { FaWhatsapp, FaCode } from 'react-icons/fa';
+import { FaWhatsapp, FaCode, FaAngleDown } from 'react-icons/fa';
 import { RxCross1 } from "react-icons/rx";
 
 
 
-let url = 'https://nextmigrant.com/timelines/gcms/'
+let url = 'https://nextmigrant.com/timelines/gcms/';
+let iframe = `<iframe allowtransparency="true" allowfullscreen="true" src="${url}?nocontent=true" frameborder="0" style="min-width:100%; max-width:100%; height:800px; border:none; background:#FFF"> </iframe>`
 const params = new URLSearchParams(window.location.search)
 export default function MainComponent() {
     const [date, setDate] = useState(moment().subtract(25, 'minute'))
     const [data, setData] = useState({})
     const [loading, setLoading] = useState(true)
     const [mF, setMF] = useState(false)
-    const [coped, setCoped] = useState(false)
+    const [embedCode, setEmbedCode] = useState(false)
+    const [copied, setCopied] = useState(false)
     const [shareOpt, setShareOpt] = useState(false)
     const [select, setSelect] = useState({
         date: 'Last_3_months',
@@ -59,12 +61,12 @@ export default function MainComponent() {
     return (
         <div className='bg-'>
             <div className='flex items-center px-4 lg:px-8 mt-10 relative'>
-                <div className='px-3 py-1 rounded-md shadow border lg:hidden'
-                    onClick={() => setMF(pre => !pre)}
-                >Filters</div>
+                <div className='text-sm px-3 py-0.5 rounded-md shadow border flex items-center lg:hidden'
+                    onClick={() => setMF(pre => !pre)}>Filters <FaAngleDown className='ml-1' />
+                </div>
 
-                <div className={`${mF ? 'flex z-[10001]' : 'hidden'} flex-col lg:flex lg:flex-row  gap-4 absolute top-4 rounded-xl shadow-2xl lg:shadow-none lg:static bg-white p-3 lg:p-0 border lg:border-0`}>
-                    <select defaultValue={'Last_4_weeks'}
+                <div className={`${mF ? 'flex z-[10001]' : 'hidden'} flex-col lg:flex lg:flex-row  gap-4 absolute top-4 rounded-xl shadow-2xl lg:shadow-none lg:static bg-white lg:bg-transparent p-3 lg:p-0 border lg:border-0`}>
+                    <select defaultValue={'Last_3_months'}
                         onChange={(e) => setSelect(pre => ({ ...pre, date: e.target.value }))}
                         id="date" className='select-box'>
                         {[
@@ -112,8 +114,8 @@ export default function MainComponent() {
                 </div>
 
                 <div className='flex flex-col lg:flex-row lg:items-center items-end gap-2 lg:gap-4 ml-auto'>
-                    <div className="text-sm lg:text-base font-medium text-[#13231A99] text-right" >
-                        Last updated:  {moment(date).fromNow()}<br className='flex lg:hidden' /> <span className="text-[#1A21FF]"
+                    <div className="text-xs lg:text-base font-medium text-[#13231A99] text-right" >
+                        Last updated:  {moment(date).fromNow()}<br className='flex lg:hidden' /> <span className="text-[#0462FE]"
                             onClick={() => {
                                 setDate(moment())
                                 setLoading(true)
@@ -121,13 +123,13 @@ export default function MainComponent() {
                             }}> Refresh data  </span>
                     </div>
 
-                    {<div className={`hidden lg:flex items-center border rounded-lg px-4 py-1.5 cursor-pointer bg-[#1A21FF] text-white relative`}
+                    {<div className={`hidden lg:flex items-center border rounded-lg px-4 py-1.5 cursor-pointer bg-[#0462FE] text-white relative`}
                         onClick={() => setShareOpt(pre => !pre)}>
                         <span className="text-base font-medium " >
                             Share
                         </span>
                         <AiOutlineShareAlt className='ml-2' size={20} color={'#ffffff'} />
-                        <div className={` flex-col gap-5 absolute bg-white top-6 right-6 md:right-10 w-72 md:w-80 p-4 rounded-lg shadow-lg z-[10001] border
+                        <div className={`flex-col gap-5 absolute bg-white top-6 right-6 md:right-10 w-72 md:w-80 p-4 rounded-lg shadow-lg z-[10001] border
                         ${shareOpt ? "flex" : "hidden"}`}>
                             <div className='flex items-center justify-between text-sm font-bold text-black'>
                                 <div>Share</div>
@@ -149,23 +151,68 @@ export default function MainComponent() {
                             <div className='flex justify-between items-center border border-[#E5E7EB] p-3 rounded-md'
                                 onClick={(e) => {
                                     copy(url)
-                                    setCoped(true)
-                                    setTimeout(() => setCoped(false), 2000)
+                                    setCopied(true)
+                                    setTimeout(() => setCopied(false), 2000)
                                     e.stopPropagation()
                                 }}>
-                                <span className=' text-xs md:text-sm font-medium text-black'>{coped ? 'Copied!' : 'Copy link'}</span>
+                                <span className=' text-xs md:text-sm font-medium text-black'>{copied ? 'Copied!' : 'Copy link'}</span>
                                 <MdCopyAll className='ml-auto cursor-pointer' size={20} color='#81724D' />
                             </div>
-                            <div className='flex justify-between items-center border border-[#E5E7EB] p-3 rounded-md'>
+                            <div className='flex justify-between items-center border border-[#E5E7EB] p-3 rounded-md'
+                                onClick={() => setEmbedCode(true)}>
                                 <span className=' text-xs md:text-sm font-medium text-black'>Embed code</span>
-                                <FaCode className='ml-auto cursor-pointer' size={20} color='#81724D'
-                                    onClick={() => copy(`<iframe allowtransparency="true" allowfullscreen="true" src="${url}?nocontent=true" frameborder="0" style="min-width:100%;max-width:100%;height:800px;border:none;background:#FFF"></iframe>`)} />
+                                <FaCode className='ml-auto cursor-pointer' size={20} color='#81724D' />
                             </div>
                         </div>
                     </div>}
-                    {(shareOpt || mF) && <div className="fixed top-0 bottom-0 left-0 right-0 z-[10000]" onClick={() => {
-                        setShareOpt(pre => !pre)
-                        setMF(pre => !pre)
+                    {embedCode &&
+                        <div className='fixed z-[10001] w-[600px] h-[300px] m-auto inset-x-0 inset-y-0 p-4 bg-white border overflow-y-scroll rounded-lg shadow-2xl px-8 py-6'>
+                            <div className='flex items-center justify-between text-base font-bold text-black'>
+                                <div>Embed this on your website</div>
+                                <RxCross1 className='mr-2 cursor-pointer' color='#202020' onClick={(e) => {
+                                    setEmbedCode(false)
+                                    e.stopPropagation()
+                                }} />
+                            </div>
+                            <code className="block rounded-lg" id='app_wizards'>
+                                {iframe}
+                            </code>
+                            <style>{`
+                            #app_wizards{
+                                color: #202020;
+                                white-space: pre-wrap;
+                                word-wrap: break-word;
+                                resize: none;
+                                background-color: rgb(242 246 252);
+                                border: 1px solid #c8ceed;
+                                width: 100%;
+                                max-height: 200px;
+                                margin: 12px 0 0;
+                                padding: 12px 16px;
+                                font-size: 14px;
+                                line-height: 24px;
+                                overflow-y: auto
+                            }
+                            `}</style>
+                            <div className='w-[120px] mt-5 mx-auto shadow  border rounded-lg px-4 py-1.5 cursor-pointer bg-[#0462FE] text-white text-center'
+                                onClick={() => {
+                                    copy(iframe)
+                                    setCopied(true)
+                                    setTimeout(() => {
+                                        setCopied(false)
+                                        setEmbedCode(false)
+                                    }, 3000)
+                                }}>
+                                <span className=' text-xs md:text-sm font-medium'>{copied ? 'Copied!' : 'Copy'}</span>
+                            </div>
+                        </div>
+                    }
+
+
+                    {(shareOpt || mF || embedCode) && <div className="fixed top-0 bottom-0 left-0 right-0 z-[10000] bg-[#20202026] backdrop-blur-[1px]" onClick={() => {
+                        setShareOpt(false)
+                        setMF(false)
+                        setEmbedCode(false)
                     }} />}
 
                 </div>
