5860b27c1a5e35e6ef79bd7ae0251847e75a6e1d
shiva-univest <shiva.singh@univest.in>
2024-10-25 12:23:31 +0530

    bug fixed

--- src/Footer.js
+++ src/Footer.js
@@ -13,18 +13,18 @@ export default function Footer({}) {
       aria-label="Univest Footer"
       className="bg-black text-white font-Inter"
     >
-      <div className="max-w-screen-xl px-4 py-16 mx-auto space-y-6 sm:px-6 lg:space-y-8 lg:px-8">
-        <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+      <div className="max-w-screen-xl px-4 py-16 mx-auto space-y-6 sm:px-6 md:space-y-8 md:px-8">
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
           <div>
             <img
               src="https://storage.googleapis.com/app-assets-univest/blogs/logo.png"
-              className="w-32 lg:w-40 object-contain"
+              className="w-32 md:w-40 object-contain"
               alt="icon"
             />
-            <p className="max-w-xs text-sm lg:text-base font-semibold mt-4">
+            <p className="max-w-xs text-sm md:text-base font-semibold mt-4">
               100% Safe & Secure.
             </p>
-            <p className="max-w-xs text-xs lg:text-sm mt-4">
+            <p className="max-w-xs text-xs md:text-sm mt-4">
               Univest encrypts all data and transactions to ensure a completely
               secure experience for our members.
             </p>
@@ -135,12 +135,12 @@ export default function Footer({}) {
             </ul>
           </div>
 
-          <div className="grid grid-cols-3 lg:col-span-2 lg:grid-cols-4">
+          <div className="grid grid-cols-3 md:col-span-2 md:grid-cols-4">
             <div>
-              <p className="font-medium text-xs lg:text-sm">PRODUCTS</p>
+              <p className="font-medium text-xs md:text-sm">PRODUCTS</p>
 
               <nav aria-label="Footer Navigation - PRODUCTS" className="mt-6">
-                <ul className="space-y-3  text-xs lg:text-smtext-sm">
+                <ul className="space-y-3  text-xs md:text-smtext-sm">
                   <li className="cursor-pointer">
                     <a href="/pro" className="transition hover:opacity-75">
                       Pro
@@ -187,10 +187,10 @@ export default function Footer({}) {
             </div>
 
             <div>
-              <p className="font-medium text-xs lg:text-sm">Company</p>
+              <p className="font-medium text-xs md:text-sm">Company</p>
 
               <nav aria-label="Footer Navigation - Company" className="mt-6">
-                <ul className="space-y-3  text-xs lg:text-smtext-sm">
+                <ul className="space-y-3  text-xs md:text-smtext-sm">
                   <li>
                     <a href="/about-us" className="transition hover:opacity-75">
                       About us
@@ -247,10 +247,10 @@ export default function Footer({}) {
             </div>
 
             <div>
-              <p className="font-medium text-xs lg:text-sm">SUPPORT</p>
+              <p className="font-medium text-xs md:text-sm">SUPPORT</p>
 
               <nav aria-label="Footer Navigation - SUPPORT" className="mt-6">
-                <ul className="space-y-3  text-xs lg:text-smtext-sm">
+                <ul className="space-y-3  text-xs md:text-smtext-sm">
                   <li>
                     <a href="/faq" className="transition hover:opacity-75">
                       FAQs
@@ -268,9 +268,9 @@ export default function Footer({}) {
 
             <nav
               aria-label="Footer Navigation - Legal"
-              className="mt-6 lg:mt-0"
+              className="mt-6 md:mt-0"
             >
-              <ul className="space-y-3  text-xs lg:text-sm">
+              <ul className="space-y-3  text-xs md:text-sm">
                 <li>
                   <img
                     src="https://storage.googleapis.com/app-assets-univest/blogs/app_download_qr.png"
@@ -323,7 +323,7 @@ export default function Footer({}) {
         <p className="text-xs ">
           &copy; 2024 Univest. All rights reserved. | Made with ❤️ in India
         </p>
-        {/* <p className="text-[10px] lg:text-xs leading-5">
+        {/* <p className="text-[10px] md:text-xs leading-5">
                     Disclaimer: Above are the only official social handles of our Company. Any communication or business transaction other than the above shared official handles by any user/customer shall not be the Company’s responsibility. User/customer(s) caution is advisable.
                 </p> */}
       </div>
@@ -336,11 +336,11 @@ export function Header({ isDark = false }) {
       id="Hero"
       className=" overflow-hidden bg-gradient-to-b from-[#343434] to-[#343434]"
     >
-      <div className=" w-full flex flex-row py-10 max-w-screen-xl mx-auto justify-between  items-center gap-20">
+      <div className="px-4 w-full flex flex-col md:flex-row py-10 max-w-screen-xl mx-auto justify-between  items-center md:gap-20">
         <div className="flex flex-col items-start gap-6">
-          <div className=" top-0 flex items-center justify-center">
+          <div className="w-full top-0 flex items-center justify-center md:justify-start">
             <div
-              className={`text-white text-[36.198px] font-bold leading-[normal]  ${
+              className={`text-white text-xl md:text-4xl font-bold ${
                 !isDark ? "text-white" : "text-[#414141]"
               }`}
             >
@@ -349,27 +349,21 @@ export function Header({ isDark = false }) {
             <img
               src="https://storage.googleapis.com/app-assets-univest/blogs/pro_light.png"
               alt="icon"
-              className="w-[78px] object-contain ml-2"
+              className="w-9 md:w-[78px] object-contain ml-2"
             />
           </div>
-          <div className="  font-Inter basis-full md:basis-7/12 flex flex-col content-start relative">
+          <div className="font-Inter basis-full md:basis-7/12 flex flex-col content-start relative">
             <span
-              className={`text-[color:var(--Pearl-White,#FFF)]  text-5xl font-extrabold leading-[80px] ${
+              className={`text-white text-xl md:text-5xl font-extrabold ${
                 !isDark ? "text-white" : "text-[#414141]"
               }`}
             >
               India’s trusted
-            </span>
-            <span
-              className={`text-[color:var(--Pearl-White,#FFF)]  text-5xl font-extrabold leading-[80px] ${
-                !isDark ? "text-white" : "text-[#414141]"
-              }`}
-            >
-              Stock market ideas
+              <br className="show-on-desktop" /> stock market ideas
             </span>
           </div>
 
-          <a href="/user/log-in" className="w-full">
+          <a href="/user/log-in" className="w-full hidden md:block">
             <button
               className={`font-Inter relative w-full text-black text-center buy-button pt-4 hover:animate-none`}
             >
@@ -393,29 +387,44 @@ export function Header({ isDark = false }) {
           />
         </div>
       </div>
-      <div className="[background:var(--Questionnaire-background-color,linear-gradient(127deg,#DDF6FF_-31.48%,#FFEAEF_95.46%))] px-4 py-2 lg:px-20 lg:py-4 flex whitespace-nowrap justify-center items-center">
-        <div className="flex items-center gap-2 lg:gap-4">
-          <p className="text-black text-sm font-medium leading-6  lg:text-[28px] lg:font-semibold lg:leading-[49px]">
+      <div className="[background:linear-gradient(127deg,#DDF6FF_-31.48%,#FFEAEF_95.46%)] px-4 py-2 md:px-20 md:py-4 flex whitespace-nowrap justify-center items-center">
+        <div className="flex items-center gap-2 md:gap-4">
+          <p className="text-black text-sm font-medium leading-6  md:text-[28px] md:font-semibold md:leading-[49px]">
             Trusted by
           </p>
-          <p className="text-black text-xl font-extrabold leading-8 lg:text-[40px] lg:font-extrabold lg:leading-[65.333px]">
+          <p className="text-black text-xl font-extrabold leading-8 md:text-[40px] md:font-extrabold md:leading-[65.333px]">
             40lakhs+ users
           </p>
         </div>
         <img
-          className="w-[116px] h-[21px] lg:w-[260px] lg:h-[48px]"
+          className="w-[116px] h-[21px] md:w-[260px] md:h-[48px] ml-2 md:ml-4"
           src="https://storage.googleapis.com/app-assets-univest/blogs/rating.png"
           alt="demo"
         />
+      </div>
+      <div className="fixed bottom-0 w-full px-4 py-3 duration-300 z-[101] shadow-[0px_0px_4px_rgba(255,255,255,0.6)] block md:hidden rounded-[12px_12px_0px_0px] proGradient">
+        <a href="/user/log-in" className="w-full">
+          <button
+            id="ProDownloadButtonClick"
+            class="select-none w-full py-1.5 rounded-full font-Inter text-base border bg-white border-primary text-black font-semibold shadow"
+            tabindex="0"
+          >
+            Activate FREE trial
+          </button>
+        </a>
       </div>
     </section>
   );
 }
 
-window?.addEventListener("load", () => {
-  ReactDOM.render(<Header />, document.getElementById("univest-top"));
-});
+try {
+  window?.addEventListener("load", () => {
+    ReactDOM.render(<Header />, document.getElementById("univest-top"));
+  });
 
-window?.addEventListener("load", () => {
-  ReactDOM.render(<Footer />, document.getElementById("univest-footer"));
-});
+  window?.addEventListener("load", () => {
+    ReactDOM.render(<Footer />, document.getElementById("univest-footer"));
+  });
+} catch (error) {
+  console.log(error);
+}
